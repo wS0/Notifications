3acbb2069b581780fa4fd12899657ff9b790eae5
Yaroslav <iponomarev@uveon.ru>
2025-11-10 16:36:22 +0300

    Add celery+flower, redis, rabbitmq
    Пока только добавлены, но не в работе за неимением логики отправки.

--- notifications/settings.py
+++ notifications/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
